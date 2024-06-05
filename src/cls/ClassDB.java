74c678d3cc9c50f54429eb220a1ab1c4ea9e5f5f
Fadilah Nur Ramadhani <fadhilar782@gmail.com>
2024-06-06 00:30:32 +0700

    first commit

---
+++ src/cls/ClassDB.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cls;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author USER
+ */
+public class ClassDB {
+    private static Connection koneksi;
+    public static Connection getkoneksi() {
+        if (koneksi==null) {
+            try {
+                String url=new String();
+                String user=new String();
+                String password=new String();
+                url="jdbc:mysql://localhost:3306/db_coba";
+                user="root";
+                password="";
+                DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+                koneksi=DriverManager.getConnection(url,user,password);
+            }catch (SQLException t) {
+                System.out.println("Error membuat koneksi");
+            }
+        }
+     return koneksi;
+    }
+}
